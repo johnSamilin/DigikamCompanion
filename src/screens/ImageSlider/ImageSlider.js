a2ec990e1754eaf8e02d95a8f6e19faad6f71974
johnSamilin <temp.kroogi@gmail.com>
2024-10-10 12:12:10 +0300

    Added share functionality (on long tap)

--- src/screens/ImageSlider/ImageSlider.js
+++ src/screens/ImageSlider/ImageSlider.js
@@ -2,23 +2,51 @@ import { SafeScreen } from '@/components/template';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '@/store';
 import Gallery from 'react-native-awesome-gallery';
+import { useRef } from 'react';
+import Share from 'react-native-share';
+import { ToastAndroid } from 'react-native';
 
 const styles = {};
 
 function ImageSlider({ route }) {
-	const { images } = useStore();
-	const uris = images.map(({ uri }) => uri);
-	const initialIndex = images.reduce((acc, image, index) => {
-		if (image.id === route.params.id) {
-			return index;
+	const { images, addLog } = useStore();
+	const currentImage = useRef(null);
+	let initialIndex = 0;
+	const uris = images.map(({ uri, id }, index) => {
+		if (id === route.params.id) {
+			initialIndex = index;
 		}
 
-		return acc;
-	}, 0);
+		return uri;
+	});
+
+	const onChangeImage = index => {
+		currentImage.current = images[index];
+	};
+
+	const onLongTap = async () => {
+		try {
+			await Share.open({
+				url: currentImage.current.uri,
+			});
+		} catch (error) {
+			addLog(error.message);
+			ToastAndroid.show(
+				'Что-то не так. см. Системные сообщения',
+				ToastAndroid.LONG,
+			);
+		}
+	};
 
 	return (
 		<SafeScreen>
-			<Gallery data={uris} initialIndex={initialIndex} numToRender={3} />
+			<Gallery
+				data={uris}
+				initialIndex={initialIndex}
+				numToRender={3}
+				onLongPress={onLongTap}
+				onIndexChange={onChangeImage}
+			/>
 		</SafeScreen>
 	);
 }
